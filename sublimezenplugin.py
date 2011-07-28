e239d26bbfb860e7e9673eb015e83b4d5f2fe15a
Nicholas Dudfield <ndudfield@gmail.com>
2011-07-28 23:12:03 +0700

    Another fix

--- sublimezenplugin.py
+++ sublimezenplugin.py
@@ -143,14 +143,15 @@ class WrapZenAsYouType(CommandsAsYouTypeBase):
 
     def run_command(self, view, cmd_input):
         try:
-            ex = expand_abbr(cmd_input)
+            ex = expand_abbr(cmd_input, super_profile='no_check_valid')
+            p = editor.get_profile_name() + '.no_check_valid'
             if not ex: raise ZenInvalidAbbreviation('Empty expansion %r' % ex)
         except ZenInvalidAbbreviation:
             return False
 
         view.run_command (
             'run_zen_action',
-            dict(action="wrap_with_abbreviation", abbr=cmd_input) )
+            dict(action="wrap_with_abbreviation", abbr=cmd_input, profile_name=p) )
 
 ################################ RUN ZEN ACTION ################################
 
