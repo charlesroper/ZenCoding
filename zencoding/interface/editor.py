1abd8065256f45c2b7ebd0e196cab7d1bf5a1cc9
Nicholas Dudfield <ndudfield@gmail.com>
2011-09-12 07:53:56 +0700

    Fix for #3

--- zencoding/interface/editor.py
+++ zencoding/interface/editor.py
@@ -33,11 +33,9 @@ class ZenEditor():
         profile_name = self.get_profile_name()
 
         if super_profile: profile_name += '.%s' % super_profile
-        
         content      = expand_abbreviation(abbr, syntax, profile_name)
 
-        return ( self.add_placeholders(content, selection=selection)
-                     .decode('utf8', 'ignore') )
+        return ( self.add_placeholders(content, selection=selection) )
 
     def __init__(self):
         pass
